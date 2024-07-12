8eaa105193a48ef74de3605fbfbe323ad9db059c
Aung Ye Htet <aung.atg1738@gmail.com>
2024-07-12 20:18:31 +0630

    update(ResponseFormat): add parameter [message]

--- src/utils/response.util.ts
+++ src/utils/response.util.ts
@@ -1,11 +1,19 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
 
-export function ResponseFormat(data: any, isSucces = true, statusCode = 200) {
-  return { data, isSucces, statusCode };
+export function ResponseFormat(
+  data: any,
+  isSucces = true,
+  statusCode = 200,
+  message = "message didn't include",
+) {
+  return { data, isSucces, statusCode, message };
 }
 
 export function HandleService(err: any) {
-  if (err instanceof BadRequestException) {
+  if (
+    err instanceof BadRequestException ||
+    err instanceof UnauthorizedException
+  ) {
     return {
       message: err.getResponse()['message'] || err.message,
       isSuccess: false,
