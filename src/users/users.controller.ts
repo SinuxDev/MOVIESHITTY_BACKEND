c6e405477b5b579250e523afeddb8df6c4b0a72f
Aung Ye Htet <aung.atg1738@gmail.com>
2024-07-12 20:21:09 +0630

    useFunc(Login): From userServices

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { createUserDTO } from './dto/createUserDTO';
+import { loginUserDTO } from './dto/loginUserDTO';
 import { HandleService } from 'src/utils/response.util';
 import { Response } from 'express';
 
@@ -38,8 +39,25 @@ export class UsersController {
     }
   }
 
-  // @Post()
-  // login(@Body(ValidationPipe) loginUserDTO: createUserDTO) {
-  //   return this.usersService.login(loginUserDTO);
-  // }
+  @Post('login')
+  @UsePipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const message = errors
+          .map((err) => Object.values(err.constraints))
+          .join(',');
+
+        return new BadRequestException({ message, isSuccess: false });
+      },
+    }),
+  )
+  async login(@Body() userLoginDTO: loginUserDTO, @Res() res: Response) {
+    try {
+      const LoginResult = await this.usersService.login(userLoginDTO);
+      return res.json(LoginResult);
+    } catch (err) {
+      const errResponse = HandleService(err);
+      return res.status(errResponse.statusCode).json(errResponse);
+    }
+  }
 }
