9f431d1144fcc414dbf72b4edca38aaa15d3ca0a
Aung Ye Htet <aung.atg1738@gmail.com>
2024-07-12 20:20:20 +0630

    create(Login): User Login Services Method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,11 +2,13 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { createUserDTO } from './dto/createUserDTO';
 import { ResponseFormat } from 'src/utils/response.util';
 import * as bcrypt from 'bcrypt';
+import { loginUserDTO } from './dto/loginUserDTO';
 
 @Injectable()
 export class UsersService {
@@ -33,7 +35,14 @@ export class UsersService {
         },
       });
 
-      return ResponseFormat(user, true, 201);
+      const { id, name, email } = user;
+
+      return ResponseFormat(
+        { id, name, email },
+        true,
+        201,
+        'User Registration Successfully',
+      );
     } catch (err) {
       throw err instanceof BadRequestException
         ? err
@@ -41,28 +50,36 @@ export class UsersService {
     }
   }
 
-  // async login(@Body(ValidationPipe) loginUser: createUserDTO) {
-  //   try {
-  //     const isUserValid = await this.databaseService.user.findUnique({
-  //       where: {
-  //         email: loginUser.email,
-  //       },
-  //     });
+  async login(userLogin: loginUserDTO) {
+    try {
+      const user = await this.databaseService.user.findUnique({
+        where: {
+          email: userLogin.email,
+        },
+      });
 
-  //     if (!isUserValid) {
-  //       throw new BadRequestException('User not valid!!');
-  //     }
+      if (!user) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
 
-  //     return response.status(200).json({
-  //       isSuccess: true,
-  //       message: 'User Login Successfully',
-  //     });
-  //   } catch (err) {
-  //     if (err instanceof BadRequestException) {
-  //       throw err;
-  //     } else {
-  //       throw new InternalServerErrorException(err);
-  //     }
-  //   }
-  // }
+      const isMatch = await bcrypt.compare(userLogin.password, user.password);
+
+      if (!isMatch) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+
+      const { id, name, email } = user;
+
+      return ResponseFormat(
+        { id, name, email },
+        true,
+        200,
+        'User Login Successfully',
+      );
+    } catch (err) {
+      throw err instanceof UnauthorizedException
+        ? err
+        : new InternalServerErrorException('An expected error occured');
+    }
+  }
 }
